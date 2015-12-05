9b653420464b9d36f894fe7239f9dfb7df7d331b
andrewstyll <andrewstyll@gmail.com>
2015-12-05 18:30:58 -0500

    completed day 5.... fuck regex's ......

--- santa5.rb
+++ santa5.rb
@@ -3,7 +3,7 @@ count_1 = 0
 count_2 = 0
 f.each_line do |line|
     line.strip!
-    if ["ab", "cd", "pq", "xy"].any? {|str| line.include?(str)} == false
+    if line.match(/^((?!ab|cd|pq|xy).)*$/)
         if line.scan(/[aeiou]/).count >= 3
              if line.scan(/(\w)\1{1,}/).count >= 1
                 count_1 += 1
@@ -11,7 +11,11 @@ f.each_line do |line|
         end
     end
 
-    
+    if line.match(/(..).*\1/)
+        if line.match(/(.).\1/)
+            count_2 += 1
+        end
+    end 
 end
 puts count_1
 puts count_2
